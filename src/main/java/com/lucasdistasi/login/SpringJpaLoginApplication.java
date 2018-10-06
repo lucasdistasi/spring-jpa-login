99105531a44fae5c90c5b977e2fd78b9ac15567d
Lucas Di Stasi <lucasdistasi@hotmail.com>
2018-10-06 14:35:32 -0300

    Initial commit

---
+++ src/main/java/com/lucasdistasi/login/SpringJpaLoginApplication.java
@@ -0,0 +1,12 @@
+package com.lucasdistasi.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaLoginApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lucasdistasi/login/SpringJpaLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucasdistasi.login;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJpaLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
