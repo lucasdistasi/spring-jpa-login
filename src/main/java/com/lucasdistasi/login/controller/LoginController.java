6b8c84fd77b184fdbe8fe50ce722a3f4eac26f36
Lucas Di Stasi <lucasdistasi@hotmail.com>
2018-10-07 17:35:01 -0300

    Modified login controller

--- src/main/java/com/lucasdistasi/login/controller/LoginController.java
+++ src/main/java/com/lucasdistasi/login/controller/LoginController.java
@@ -1,14 +1,28 @@
 package com.lucasdistasi.login.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
-	
-	@GetMapping(value="/login")
-	public String login(Model model) {
+
+	@GetMapping(value = "/login")
+	public String login(@RequestParam(value = "error", required = false) String error, Model model, Principal principal,
+			RedirectAttributes ra) {
+		if (principal != null) {
+			return "redirect:/home";
+		}
+
+		if (error != null) {
+			ra.addFlashAttribute("login_error", true);
+			return "redirect:/login";
+		}
+
 		return "login";
 	}
 
